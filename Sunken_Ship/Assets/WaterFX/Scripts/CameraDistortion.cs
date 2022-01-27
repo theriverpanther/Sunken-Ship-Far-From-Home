f26b023d98fc89e7648ce12de36cffc7ccc49c5e
David Smith (RIT Student) <theriverpanther@gmail.com>
2022-01-27 10:09:32 -0500

    Implemented Camera Distortion
    Handled programatically distorting the camera lens through post processing to add a wave effect. TODO: Test other equations to make sure that we get as close to the effect as we want, as well as make sure that we make this as safely accessible as possible.

--- Sunken_Ship/Assets/WaterFX/Scripts/CameraDistortion.cs
+++ Sunken_Ship/Assets/WaterFX/Scripts/CameraDistortion.cs
@@ -6,37 +6,61 @@ using UnityEngine.Rendering.PostProcessing;
 public class CameraDistortion : MonoBehaviour
 {
     [SerializeField]
+    PostProcessProfile profile;
+    [SerializeField]
     LensDistortion distortion;
     
     public float speed;
     public bool invert;
     
     [Range (-1.0f,0.0f)]
-    float minX;
+    public float minX;
     [Range (0.0f, 1.0f)]
-    float maxX;
+    public float maxX;
     [Range (-1.0f,0.0f)]
-    float minY;
+    public float minY;
     [Range (0.0f, 1.0f)]
-    float maxY;
+    public float maxY;
+
+    private float xRange;
+    private float yRange;
+
+    private float timeRan = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        profile.TryGetSettings(out distortion);
+        xRange = maxX - minX;
+        yRange = maxY - minY;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // TODO: Sine Wave
+        float x;
+        float y;
+        // Move clockwise
         if(invert)
         {
-            distortion.centerX.value = Mathf.Clamp(Mathf.Cos(speed * Time.deltaTime), minX, maxX));
-            distortion.centerY.value = Mathf.Clamp(Mathf.Sin(speed * Time.deltaTime), minY, maxY));
+            x = Mathf.Clamp(Mathf.Cos(speed * timeRan), minX, maxX) * xRange;
+            y = Mathf.Clamp(Mathf.Sin(speed * timeRan), minY, maxY) * yRange;
+            
         }
+        // Move counter-clockwise
         else
         {
-            distortion.centerX.value = Mathf.Clamp(Mathf.Sin(speed * Time.deltaTime), minX, maxX));
-            distortion.centerY.value = Mathf.Clamp(Mathf.Cos(speed * Time.deltaTime), minY, maxY));
+            x = Mathf.Clamp(Mathf.Sin(speed * timeRan), minX, maxX) * xRange;
+            y = Mathf.Clamp(Mathf.Cos(speed * timeRan), minY, maxY) * yRange;
         }
+        // Adjust the filter values
+        distortion.centerX.value = x;
+        distortion.centerY.value = y;
+
+        // Show the filter values on the console
+        Debug.Log("(" + x + ", " + y + ")");
+
+        // Add to the timer
+        timeRan += Time.deltaTime;
     }
 }
