451fbddb09b359609b660c9a346c7412912d4575
Xander Goodwin <xgoodwin@idtech.com>
2022-02-12 19:34:37 -0500

    Added gravity to movement and the model to the player character

--- Sunken_Ship/Assets/Scripts/PlayerController.cs
+++ Sunken_Ship/Assets/Scripts/PlayerController.cs
@@ -6,35 +6,41 @@ public class PlayerController : MonoBehaviour
 {
     //Fields
     Rigidbody rb;
-    [SerializeField] float forceVector;
+    //[SerializeField] float forceVector;
+    [SerializeField] float velocity = 0;
+    [SerializeField] Vector3 test;
+    const float acceleration = 0.05f;
+    const float maxVelocity = 8.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
-        rb.maxAngularVelocity = 1.5f;
+        //rb.maxAngularVerblocity = 1.5f;
         rb.maxDepenetrationVelocity = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        forceVector = Mathf.Sqrt(Mathf.Pow(rb.velocity.x, 2) + Mathf.Pow(rb.velocity.x, 2)); //Distance Formula
+        //forceVector = Mathf.Sqrt(Mathf.Pow(velocity, 2) + Mathf.Pow(velocity, 2)); //Distance Formula
 
         //Rotate
         if (Input.GetKey(KeyCode.A))
         {
             rb.AddTorque(new Vector3(0, -0.15f, 0));
+            transform.Rotate(0, -0.15f, 0);
         }
         if (Input.GetKey(KeyCode.D))
         {
             rb.AddTorque(new Vector3(0, 0.15f, 0));
+            transform.Rotate(0, 0.15f, 0);
         }
 
         //Slow Angular Velocity
         if(!Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
         {
-            rb.AddTorque(new Vector3(0, rb.angularVelocity.y * -0.1f, 0));
+            rb.AddTorque(new Vector3(0, rb.angularVelocity.y * -0.01f, 0));
 
             if(Mathf.Abs(rb.angularVelocity.y) < 0.5)
             {
@@ -42,24 +48,36 @@ public class PlayerController : MonoBehaviour
             }
         }
 
+
+        //Movement
+        rb.velocity = new Vector3(transform.forward.x * velocity, rb.velocity.y, transform.forward.z * velocity);
+        //test = Vector3.Normalize(transform.forward) * velocity;
+        //transform.position += Vector3.Normalize(transform.forward) * velocity;
+
         //Move Forward
         if (Input.GetKey(KeyCode.W))
         {
-            rb.AddForce(transform.forward * 2);
-            if(rb.velocity.magnitude > 5)
+            if(velocity < maxVelocity)
             {
-                rb.velocity = rb.velocity.normalized * 5;
+                velocity += acceleration;
+            }
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            if (velocity < maxVelocity)
+            {
+                velocity -= acceleration;
             }
         }
         else
         {
-            if(Mathf.Abs(rb.velocity.x) > 0)
+            if(velocity > 0)
             {
-                rb.AddForce(new Vector3(rb.velocity.x * -0.3f, 0, 0));
+                velocity -= (acceleration / 4);
             }
-            if (Mathf.Abs(rb.velocity.z) > 0)
+            if (velocity < 0)
             {
-                rb.AddForce(new Vector3(0, 0, rb.velocity.z * -0.3f));
+                velocity += (acceleration / 4);
             }
         }
     }
