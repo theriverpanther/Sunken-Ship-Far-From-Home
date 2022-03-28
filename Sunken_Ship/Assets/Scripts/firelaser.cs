5fdd529077ab35ac8e15bc4de3b14600b244936d
Raj Barot <rb9769@g.rit.edu>
2022-03-27 21:22:37 -0400

    a

---
+++ Sunken_Ship/Assets/Scripts/firelaser.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firelaser : MonoBehaviour 
+{
+    private float lastfire;
+    public float firerate;
+    public GameObject laserprefab;
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButton("Fire1") && Time.time > lastfire + firerate)
+        {
+            lastfire = Time.time;
+            fireslaser();
+
+        }
+            
+
+    }
+
+    void fireslaser()
+    {
+        GameObject laser = GameObject.Instantiate(laserprefab, transform.position, transform.rotation) as GameObject;  
+    }
+}
