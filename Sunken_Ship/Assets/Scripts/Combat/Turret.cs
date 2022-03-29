5247947c30ccb0555b4a9a072566ab614c2a99d4
ps4100-Knight <ps4100@g.rit.edu>
2022-03-29 03:53:04 -0400

    fianlly building now

--- Sunken_Ship/Assets/Scripts/Combat/Turret.cs
+++ Sunken_Ship/Assets/Scripts/Combat/Turret.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
@@ -10,7 +12,7 @@ public class Turret : MonoBehaviour
 
     void OnDrawGizmos()
     {
-#if UNITY_EDITOR
+//#if UNITY_EDITOR
         if (!target) return;
 
         var dashLineSize = 2f;
@@ -22,21 +24,21 @@ public class Turret : MonoBehaviour
             var projection = Vector3.ProjectOnPlane(target.position - hardpoint.position, hardpoint.up);
 
             // projection line
-            Handles.color = Color.white;
-            Handles.DrawDottedLine(target.position, hardpoint.position + projection, dashLineSize);
+          //  Handles.color = Color.white;
+          //  Handles.DrawDottedLine(target.position, hardpoint.position + projection, dashLineSize);
 
             // do not draw target indicator when out of angle
             if (Vector3.Angle(hardpoint.forward, projection) > mountPoint.angleLimit / 2) return;
 
             // target line
-            Handles.color = Color.red;
-            Handles.DrawLine(hardpoint.position, hardpoint.position + projection);
+          //  Handles.color = Color.red;
+          //  Handles.DrawLine(hardpoint.position, hardpoint.position + projection);
 
             // range line
-            Handles.color = Color.green;
-            Handles.DrawWireArc(hardpoint.position, hardpoint.up, from, mountPoint.angleLimit, projection.magnitude);
-            Handles.DrawSolidDisc(hardpoint.position + projection, hardpoint.up, .5f);
-#endif
+          //  Handles.color = Color.green;
+         //   Handles.DrawWireArc(hardpoint.position, hardpoint.up, from, mountPoint.angleLimit, projection.magnitude);
+         //   Handles.DrawSolidDisc(hardpoint.position + projection, hardpoint.up, .5f);
+//#endif
         }
     }
 
