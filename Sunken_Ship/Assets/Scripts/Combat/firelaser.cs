5389a332bdc34f080eb40d87854af7905857e5c8
Xander Goodwin <xcg2903@rit.edu>
2022-04-08 16:23:21 -0400

    Made flare able to be shot by player (PRESS Q TO SWITCH)

--- Sunken_Ship/Assets/Scripts/Combat/firelaser.cs
+++ Sunken_Ship/Assets/Scripts/Combat/firelaser.cs
@@ -7,25 +7,49 @@ public class firelaser : MonoBehaviour
     private float lastfire;
     public float firerate;
     public GameObject laserprefab;
+    public GameObject flarePrefab;
 
-   
+    //Swap between
+    public bool useFlare = true;
 
     // Update is called once per frame
     void Update()
     {
-
         if (Input.GetButton("Fire1") && Time.time > lastfire + firerate)
         {
             lastfire = Time.time;
-            fireslaser();
 
+            if (useFlare)
+            {
+                FireFlare();
+            }
+            else
+            {
+                fireslaser();
+            }
         }
-            
 
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (useFlare)
+            {
+                useFlare = false;
+            }
+            else
+            {
+                useFlare = true;
+            }
+        }
     }
 
     void fireslaser()
     {
         GameObject laser = GameObject.Instantiate(laserprefab, transform.position, transform.rotation) as GameObject;  
+    }
+
+    //Added Flare
+    void FireFlare()
+    {
+        GameObject flare = GameObject.Instantiate(flarePrefab, transform.position, transform.rotation) as GameObject;
     }
 }
